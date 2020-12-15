2bd5673e7579f6846f03d5ea9f4c78356ae2313b
mmmys <74124890+mmmys@users.noreply.github.com>
2020-12-15 19:58:44 +0800

    下载试用

---
+++ text2.py
@@ -0,0 +1,9 @@
+filename='programming.txt'
+with open(filename,'w') as file_object:
+    file_object.write('I love programming.\n')
+    file_object.write('I love creating new games.\n')
+
+filename='programming.txt'
+with open(filename,'a') as file_object:
+    file_object.write('I also love programming csav vava wf.\n')
+    file_object.write('I love creating new gamesa d fsd hydas.\n')
